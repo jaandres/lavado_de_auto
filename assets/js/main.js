0b796cc4084c4eb2a530f64c3c1077c777c875f0
DouglasGuacaran <douglasguacaran@gmail.com>
2022-01-20 16:30:56 -0300

    ultimos avances

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,14 @@ $(document).ready(function () {
     $("#formulario").validate({
         rules: {
           rut:{
-              required: true,
-              number: true,
-              minlength: 3,
-              maxlength: 8,
+            required: true,
+            number: true,
+            minlength: 3,
+            maxlength: 8,
           },
           digito: {
             minlength: 1,
             maxlength: 1,
-            range: [1,8],
             required: true,
           },
           nombre: {
@@ -50,7 +49,7 @@ $(document).ready(function () {
         messages : { 
           rut : { 
             required:'Escriba el rut', 
-            number:'Sólo deben ser números', 
+            number:'Sólo deben ser números sin punton ni guión', 
             minlength:'Por favor ingrese al menos tres caracteres', 
             maxlength:'debe ingresar el RUT sin dígito verificador'
           },
@@ -60,17 +59,15 @@ $(document).ready(function () {
             range: 'El número no debe ser mayor a ocho',
             required: 'debe ingresar un dígito verificador',
           },
-
           nombre: {
             required: "Ingrese su nombre",
             minlength: "Debe tener mas de 3 caracteres",
             maxlength: "No puede tener mas de 20 caracteres",
-            digits: 'solo se permiten caracteres',
           },
           apellido: {
             required: "Ingrese su apellido",
             minlength: "Debe tener mas de 3 caracteres",
-            maxlength: "No puede tener mas de 20 caracteres"
+            maxlength: "No puede tener mas de 20 caracteres",
           },
           direccion: {
             required: "Ingrese su dirección",
@@ -78,45 +75,92 @@ $(document).ready(function () {
             maxlength: "No puede tener mas de 80 caracteres",
           },
           marca: {
-            required: "Ingrese su dirección",
+            required: "Ingrese la marca del vehículo",
             minlength: "Debe tener mas de 3 caracteres",
             maxlength: "No puede tener mas de 15 caracteres",
           },
           modelo: {
-            required: "Ingrese su dirección",
+            required: "Ingrese el modelo del vehículo",
             minlength: "Debe tener mas de 3 caracteres",
             maxlength: "No puede tener mas de 15 caracteres",
           },
           año: {
             required: "Ingrese un año valido",
-            año: "Ingrese un año",
+            number: "Ingrese un año",
             minlength: "Debe tener mas de 4 caracteres",
             maxlength: "No puede tener mas de 4 caracteres",
           }
-          
+        },
+  submitHandler: function (formulario) {
+    
+    console.log("entro a funcion")
+    if (
+      $("#digito").val() == "k" ||
+      $("#digito").val() <= 8 &&
+      $("#digito").val() >= 0
+    ) {
+        let mensaje = document.getElementById("error");
+        if ( $("#digito").val() >= 0 ||  $("#digito").val() == ""){          
+            mensaje.innerHTML = "";
+            alert('Ingreso de orden correcto')
+            // $('#formulario')[0].reset();
         }
-    })
-    var userIdExistentes = [
-        "17273455",
-        "17272455",
-        "17273455",
-        "17073455",
-        "17273455",
-        "17673455",
-        "17273455",
-        "20473455",
-        "20873455",
-        "20273455",
-        "20073455",
-        "20300455",
-        "20273455",
-        "26349213",
-        "8848609",
-        "8400609",
-        "8666609",
-        "8234609",
-      ];
-      $("#rut").autocomplete({
-        source: userIdExistentes
-      });
+    } else {
+      let mensaje = document.getElementById("error");
+      alert('Error intente de nuevo')
+      mensaje.innerHTML = "ingrese un digito del 0 a el 8 o una letra k";
+    }
+  }
+  })
+  var userIdExistentes = [
+      "17273455",
+      "17272455",
+      "17273455",
+      "17073455",
+      "17273455",
+      "17673455",
+      "17273455",
+      "20473455",
+      "20873455",
+      "20273455",
+      "20073455",
+      "20300455",
+      "20273455",
+      "26349213",
+      "8848609",
+      "8400609",
+      "8666609",
+      "8234609",
+    ];
+  $("#rut").autocomplete({
+    source: userIdExistentes
+  });
+
+  addEventListener("click",function(){
+    const iva = 0.19;
+    let total = document.getElementById("total");
+    let costo = document.getElementById("costo");
+    let lavado_ext_int = document.getElementById("lavado_ext_int");
+    let lavado_motor = document.getElementById("lavado_motor");
+    let valor_lavado_ext_int = 7000
+    let valor_motor = 15000
+
+    if (lavado_ext_int.checked && lavado_motor.checked){
+      total.value = [(valor_lavado_ext_int+(valor_lavado_ext_int*iva))+(valor_motor+(valor_motor*iva))]
+      costo.value = valor_lavado_ext_int + valor_motor 
+    } else if ( lavado_ext_int.checked && lavado_motor.unchecked){
+      total.value = valor_lavado_ext_int+(valor_lavado_ext_int*iva);
+      costo.value = valor_lavado_ext_int
+    }else if( lavado_motor.checked){
+      total.value = valor_motor+(valor_motor*iva);
+      costo.value = valor_motor
+    } else if (lavado_ext_int.unchecked){
+      total.innerHTML = "Valor neto del servicio";
+      costo.innerHTML = "Total mas IVA";
+    } else {
+      total.innerHTML = "Valor neto del servicio";
+      costo.innerHTML = "Total mas IVA";
+    }
+
+  });
 });
